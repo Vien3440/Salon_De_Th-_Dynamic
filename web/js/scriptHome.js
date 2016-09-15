5bedd642929d6a3f2c5c268ec573c71f6c28439a
vivien <vivien34400@laposte.net>
2016-09-16 01:43:35 +0200

    implémentation des couleurs ("_general.scss") et avancer sextion home

--- web/js/scriptHome.js
+++ web/js/scriptHome.js
@@ -1,1 +1,9 @@
 $("header").css("height", window.innerHeight + 100 ); // Header Taille 
+
+
+$("#tof").css("height", window.innerHeight); // tof Taille 
+
+$("#tof").css("padding-top", window.innerHeight / 6); // tof Taille 
+
+
+$("#text").css("height", window.innerHeight); // tof Taille 
